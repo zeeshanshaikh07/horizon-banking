ce8d7dc9d56efce54a80914880c99740c5c71577
Zeeshan Shaikh <zeeshanshaikh790.zs@gmail.com>
2024-10-19 17:19:39 +0530

    install and inital plaid setup

--- components/ui/AuthFrom.tsx
+++ components/ui/AuthFrom.tsx
@@ -23,6 +23,7 @@ import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
+import PlaidLink from "./PlaidLink";
 
 const AuthFrom = ({ type }: { type: string }) => {
   const router = useRouter();
@@ -90,7 +91,9 @@ const AuthFrom = ({ type }: { type: string }) => {
         </div>
       </header>
       {user ? (
-        <div className="flex flex-col gap-4">{/* PlaidLink */}</div>
+        <div className="flex flex-col gap-4">
+          <PlaidLink user={user} variant="primary" />
+        </div>
       ) : (
         <>
           <Form {...form}>
